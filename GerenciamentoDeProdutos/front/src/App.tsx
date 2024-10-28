74e4384b235f2581c692b2d387111701947ac41e
Drieko <driekodev@gmail.com>
2024-10-28 20:45:20 -0300

    test

--- GerenciamentoDeProdutos/front/src/App.tsx
+++ GerenciamentoDeProdutos/front/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        <H1>poggers</H1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
